a231ce076076a0f2259b60a906a22096602aaae3
nguyenanhung <info@nguyenanhung.com>
2024-03-15 00:08:03 +0700

    Update at 2024-03-15 00:08:03 on Mac-mini-M1--Home

--- src/DataRepository.php
+++ src/DataRepository.php
@@ -11,36 +11,36 @@
 namespace nguyenanhung\Libraries\String;
 
 if (!class_exists('nguyenanhung\Libraries\String\DataRepository')) {
-    /**
-     * Class DataRepository
-     *
-     * @package   nguyenanhung\Libraries\String
-     * @author    713uk13m <dev@nguyenanhung.com>
-     * @copyright 713uk13m <dev@nguyenanhung.com>
-     */
-    class DataRepository
-    {
-        const CONFIG_PATH = 'config';
-        const CONFIG_EXT = '.php';
+	/**
+	 * Class DataRepository
+	 *
+	 * @package   nguyenanhung\Libraries\String
+	 * @author    713uk13m <dev@nguyenanhung.com>
+	 * @copyright 713uk13m <dev@nguyenanhung.com>
+	 */
+	class DataRepository
+	{
+		const CONFIG_PATH = 'config';
+		const CONFIG_EXT = '.php';
 
-        /**
-         * Hàm lấy nội dung config được quy định trong thư mục config
-         *
-         * @author: 713uk13m <dev@nguyenanhung.com>
-         * @time  : 9/28/18 14:47
-         *
-         * @param string $configName Tên file config
-         *
-         * @return array|mixed
-         */
-        public static function getData($configName)
-        {
-            $path = __DIR__ . DIRECTORY_SEPARATOR . self::CONFIG_PATH . DIRECTORY_SEPARATOR . trim($configName) . self::CONFIG_EXT;
-            if (is_file($path) && file_exists($path)) {
-                return require $path;
-            }
+		/**
+		 * Hàm lấy nội dung config được quy định trong thư mục config
+		 *
+		 * @param string $configName Tên file config
+		 *
+		 * @return array|mixed
+		 * @author: 713uk13m <dev@nguyenanhung.com>
+		 * @time  : 9/28/18 14:47
+		 *
+		 */
+		public static function getData($configName)
+		{
+			$path = __DIR__ . DIRECTORY_SEPARATOR . self::CONFIG_PATH . DIRECTORY_SEPARATOR . trim($configName) . self::CONFIG_EXT;
+			if (is_file($path) && file_exists($path)) {
+				return require $path;
+			}
 
-            return array();
-        }
-    }
+			return array();
+		}
+	}
 }
